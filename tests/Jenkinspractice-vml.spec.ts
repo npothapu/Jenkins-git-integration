5f32fa165ee9b68c1ca53d04f6186c8a0620d3f8
nandakumar.pothapu@vml.com <nandakumar.pothapu@vml.com>
2025-01-08 16:37:34 -0500

    test auto to show charlotte  build  jenkins

--- tests/Jenkinspractice-vml.spec.ts
+++ tests/Jenkinspractice-vml.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test';
 
-// test('has title', async ({ page }) => {
-//   await page.goto('https://www.vml.com/');
-//   await page.getByRole('button', { name: 'Accept All Cookies' }).click();
-//   await page.getByRole('link', { name: 'About Us' }).click();
-//   await expect(page.locator('h1')).toContainText('About Us');
-//   await page.getByRole('link', { name: 'Work', exact: true }).click();
-//   await expect(page.locator('h1')).toContainText('Our Work');
-// console.log("test 1 vml good");
-// });
+test('has title', async ({ page }) => {
+  await page.goto('https://www.vml.com/');
+  await page.getByRole('button', { name: 'Accept All Cookies' }).click();
+  await page.getByRole('link', { name: 'About Us' }).click();
+  await expect(page.locator('h1')).toContainText('About Us');
+  await page.getByRole('link', { name: 'Work', exact: true }).click();
+  await expect(page.locator('h1')).toContainText('Our Work');
+console.log("test 1 vml good");
+});
 
 test('get started link', async ({ page }) => {
   await page.goto('https://www.vml.com/');
